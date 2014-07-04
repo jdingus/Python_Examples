177200106a9084e91f6e4f04df4a8bcb96dfee3f
Josh <josh.dingus@gmail.com>
2014-07-04 10:48:42 -0400

    json example

--- parse.py
+++ parse.py
@@ -1,5 +1,6 @@
 from collections import OrderedDict
 import json
+import pprint # used to print unicode files
 
 # Nice way to visualize data
 with open('data.json') as json_file:
@@ -22,15 +23,36 @@ print "\n"
 print "-----" * 4
 print "\n"
 
+# """Another option to use is the PrettyPrinter class"""
+# pp = pprint.PrettyPrinter(indent=4)  #create instance of class first to use
+# pp.pprint(json_data)
+
+# print "\n"
+# print "-----" * 4
+# print "\n"
+
 # All uls
 with open('data.json') as json_file:
 	json_data = json.load(json_file)
 
+#print len(json_data[0]['trends']) # 'trends' is a list of dicts print # entries
+
+def 
 	for data in json_data:
+		i=1
 		for value in json_data[0]['trends']:
-			print value['url']
-
-
-print "\n"
-print "-----" * 4
-print "\n"
+			# pp.pprint(value["name"] + " : " + value["url"] )
+			name_tweet =  value["name"].encode('utf-8')
+			url_tweet = value['url'].encode('utf-8')
+			print 'This is the {} entry in the list.'.format('#'+str(i))
+			print 'The name of the tweet is : {}'.format(name_tweet)
+			print 'The url is : {}'.format(url_tweet)
+			print '\n'
+			print '------' *4
+			i+=1
+"""
+1. Start with entire JSON object
+2. Since it's wrapped in a list, iterate through it with a for loop
+3. Next, work your way to the inner key ('trends') that contains the url
+4. Loop through the values and output the 'url'
+"""
